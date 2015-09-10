c36786293398ad14915a09a4a6604f11e81bcc0c
Shuvalov Anton <anton_shuvalov@me.com>
2015-09-10 23:42:13 +0300

    update & bugfixes

--- bot.js
+++ bot.js
@@ -16,16 +16,12 @@ exports.logMessage = function(t) {
   console.log(c.blue('@' + name) + ': ' + message);
 }
 
-exports.logReply = function(t) {
-  var name = t.user.screen_name;
-  console.log(c.red('@your_mommy: '), '@' + name + ' ' + exports.reply(t));
-}
-
 exports.reply = function(t) {
-  var message = t.text;
+  var name = t.user.screen_name;
+  var message = t.text.toLowerCase();
   var phrases = replies.keys.reduce(function(memo, key) {
     if (!~message.indexOf(key)) return memo;
     return memo.concat(replies[key]);
   }, []);
-  return phrases[Math.floor(Math.random() * phrases.length)] || 'ну ладн чо!';
+  return  '@' + name + ' ' + (phrases[Math.floor(Math.random() * phrases.length)] || 'ну ладн чо!');
 }
--- index.js
+++ index.js
@@ -15,14 +15,18 @@ var client = new Twitter(tokens)
 client.stream('statuses/filter', { track: replies.keys.join(',') }, function(stream){
   stream
     .on('data', function(t) {
+      if(~t.text.indexOf('RT')) return;
+      if(t.user.screen_name === 'your_mommy') return;
       bot.logMessage(t);
-      bot.logReply(t);
+      client.post('statuses/update', {
+        status: bot.reply(t),
+        in_reply_to_status_id: t.id_str
+      }, function(err, tweet) {
+        console.log(tweet.text);
+      });
     })
     .on('end', function(r) {
       console.log('Bye.');
-    })
-    .on('error', function(error) {
-      console.log('Error: ' + (error.code ? error.code + ' ' + error.message : error.message));
     })
   ;
 });
