b405fd37f8accf35f8059ddf5ff46f79468d7809
Shuvalov Anton <anton_shuvalov@me.com>
2015-09-10 22:45:08 +0300

    update bot to create correct replies

---
+++ bot.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * Dependencies
+ */
+var c = require('chalk');
+var replies = require('./replies');
+
+
+/**
+ * Log message for debug
+ */
+exports.logMessage = function(t) {
+  var name = t.user.screen_name;
+  var message = t.text;
+  console.log(c.blue('@' + name) + ': ' + message);
+}
+
+exports.logReply = function(t) {
+  console.log(c.red('@your_mommy: '), '@' + name + ' ' + exports.reply(t));
+}
+
+exports.reply = function(t) {
+  var name = t.user.screen_name;
+  var message = t.text;
+  var phrases = replies.keys.reduce(function(memo, key) {
+    if (!~message.indexOf(key)) return memo;
+    return memo.concat(replies[key]);
+  }, []);
+  return phrases[Math.floor(Math.random() * phrases.length)] || 'ну ладн чо!';
+}
---
+++ replies.js
@@ -0,0 +1,16 @@
+/**
+ * Replies for keywords
+ */
+module.exports = exports = {
+  'говно': [
+    'у тебя в штанах говно!!!',
+    'говно у тебя в штанах!',
+    'в твоих штанах говно!!1',
+    'говно в твоих штанах говно!!1',
+  ]
+};
+
+/**
+ * Keywords
+ */
+exports.keys = Object.keys(exports);
